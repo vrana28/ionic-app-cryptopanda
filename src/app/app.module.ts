baf98f3dae70d13a07da6b19151bd4e9ec180d5e
vrana28 <vranjesdjordje@gmail.com>
2021-05-08 19:52:45 +0200

    Inserted ion-cards with element of cryptocurrencies where you can see data as id, name, price, logo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {HomePage} from './home/home.page';
+import {DataService} from './data.service';
+import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    IonicModule.forRoot(),
+    AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },{provide: DataService}],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
